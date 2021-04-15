99b0f7372b3887a77b4fbeb4d3be124d6b9bd75f
Sarto Gagnon <Sarto.Gagnon@ssq.ca>
2021-04-14 23:34:44 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to the Sartos and nachos!'));
+app.get('/', (req, res) => res.send('This tutorial was very cool'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
